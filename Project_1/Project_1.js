710cb07139dde2daf75677aecff8d75cbd6ad783
Isaac Pure <isaacpure@gmail.com>
2022-07-27 21:02:30 -0500

    functional buttons, background, basic CSS edit

--- Project_1/Project_1.js
+++ Project_1/Project_1.js
@@ -1,75 +1,44 @@
-// ARRAYS TO BE USED 
-const whereWorked = ["Work 1", "Work 2", "Work 3", "Work 4"]
-const myProjects = ["Project 1", "Project 2", "Project 3"]
-const contactInfoSubmit = ["isaacpure@gmail.com", "https://www.linkedin.com/in/isaacpure/"]
-
 // START JQUERY AND JS CODE
 $(() => {
-// CREATES AND APPENDS ULs, THEN HIDES THEM IMMEDIATELY UPON LOAD
-    const $resume_ul = $("<ul>").attr("id", "resume-ul");
-     $("nav").append($resume_ul);
-     $resume_ul.hide();
+// GRABS IDs FROM ALL DIFFERENT SECTIONS AND HIDES EXCEPT BIO UPON LOAD
+    const $hideResume_id = $("#resume").hide()
+    const $hideProject_id = $("#projects").hide()
+    const  $hideContact_id = $("#contact-info").hide()
 
-    const $project_ul = $("<ul>").attr("id", "project-ul");
-    $("nav").append($project_ul);
-    $project_ul.hide();
 
-    const $contactSubmit_1 = $("<p>").text(contactInfoSubmit[0]).addClass("contact");
-    $("nav").append($contactSubmit_1);
-    $contactSubmit_1.hide();
-
-    const $contactSubmit_2 = $("<p>").text(contactInfoSubmit[1]).addClass("contact");
-    $("nav").append($contactSubmit_2);
-    $contactSubmit_2.hide();
- 
-// FOR LOOP THAT CREATES THE LIs WITH RESUME POINTS
-    for (let i = 0; i < whereWorked.length; i++) {
-        const $resume_li = $("<li>").css("background", "red");
-        $resume_li.addClass("resume_bullet").text(whereWorked[i]);
-        $($resume_ul).append($resume_li)
-    } 
-    
-    for (let i = 0; i < myProjects.length; i++) {
-        const $project_li = $("<li>").css("background", "blue");
-        $project_li.addClass("project_bullet").text(myProjects[i]);
-        $($project_ul).append($project_li)
-    } 
-
-// UPON CLICK HIDES THE ABOUT ME PORTION
+// // UPON CLICK HIDES THE ABOUT ME PORTION
     
     const clickResume = () => {
         $("#aboutme").hide(1000);
-        $("#project-ul").hide(1000);
-        $(".contact").hide(1000);
-        $resume_ul.show(900);
+        $("#projects").hide(1000);
+        $("#contact-info").hide(1000);
+        $("#resume").show(900);
     }
        
     const clickHome = () => {
-        $("#resume-ul").hide(1000);
-        $("#project-ul").hide(1000);
-        $(".contact").hide(1000);
+        $("#resume").hide(1000);
+        $("#projects").hide(1000);
+        $(".contact_info").hide(1000);
         $("#aboutme").show(900)
 
 
     }
     const clickProject = () => {
         $("#aboutme").hide(1000);
-        $("#resume-ul").hide(1000);
-        $(".contact").hide(1000);
-        $project_ul.show(900)
+        $("#resume").hide(1000);
+        $("#contact-info").hide(1000);
+        $("#projects").show(900);
     }
-
     const clickContact = () => {
         $("#aboutme").hide(1000);
-        $("#resume-ul").hide(1000);
-        $("#project-ul").hide(1000);
-        $contactSubmit_1.show(900)
-        $contactSubmit_2.show(900)
+        $("#resume").hide(1000);
+        $("#projects").hide(1000);
+        $("#contact-info").show(900);
     }
 
-$("#resume").on('click', clickResume);
-$("#home").on('click', clickHome);
-$("#project").on('click', clickProject);
-$("#contact").on('click', clickContact);
+$("#resumeButton").on('click', clickResume);
+$("#homeButton").on('click', clickHome);
+$("#projectButton").on('click', clickProject);
+$("#contactButton").on('click', clickContact);
 
 })
