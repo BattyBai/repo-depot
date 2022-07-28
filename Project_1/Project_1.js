c798832f8d65363f02398e2c795c910af2980743
Isaac Pure <isaacpure@gmail.com>
2022-07-28 17:55:44 -0500

    Project 1 Redux

--- Project_1/Project_1.js
+++ Project_1/Project_1.js
@@ -1,26 +1,26 @@
 // START JQUERY AND JS CODE
 $(() => {
 // GRABS IDs FROM ALL DIFFERENT SECTIONS AND HIDES EXCEPT BIO UPON LOAD
-    const $hideResume_id = $("#resume").hide()
-    const $hideProject_id = $("#projects").hide()
-    const  $hideContact_id = $("#contact-info").hide()
+    // const $hideResume_id = $("#resume").hide()
+    // const $hideProject_id = $("#projects").hide()
+    // const  $hideContact_id = $("#contact-info").hide()
 
 
 // // UPON CLICK HIDES THE ABOUT ME PORTION
     
     const clickResume = () => {
+        $("#resume").show(900);
         $("#aboutme").hide(1000);
         $("#projects").hide(1000);
         $("#contact-info").hide(1000);
-        $("#resume").show(900);
+        
     }
        
     const clickHome = () => {
+        $(".contact_info").hide(1000);
         $("#resume").hide(1000);
         $("#projects").hide(1000);
-        $(".contact_info").hide(1000);
         $("#aboutme").show(900)
-
 
     }
     const clickProject = () => {
