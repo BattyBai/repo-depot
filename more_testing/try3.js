8a22e20bb96164ffa0555454ca39b19751741dc0
Isaac Pure <isaacpure@gmail.com>
2022-07-30 16:53:14 -0500

    button highlight and more functionality

--- more_testing/try3.js
+++ more_testing/try3.js
@@ -1,5 +1,10 @@
 // START JQUERY AND JS CODE
 $(() => {
+
+$(".resume").hide()
+$(".about-me").hide()
+$(".project").hide()
+    // BUTTON FUNCTIONALITY. WHEN CLICLING, SLIDES UP OR DOWN (HIDE/SHOWS) THE CURRENT TOPIC CLICKED 
 const clickHome = () => {
     $(".resume").slideUp(1000);
     $(".about-me").slideUp(1000);
@@ -9,12 +14,44 @@ const clickAboutMe = () => {
     $(".about-me").slideDown(900);
 }
 const clickResume = () => {
-$(".about-me").slideUp(1000);
-$(".resume").slideDown(900);
+    $(".about-me").slideUp(1000);
+    $(".resume").slideDown(900);
 }
+
+const $hoverColor= () => {
+    $(".topbutton").hover(
+        function(){$(this).css("background", "gray");
+    },
+        function(){$(this).css("background","transparent");
+    });
+    $(".push").hover(
+        function(){$(this).css("background", "gray");
+    },
+        function(){$(this).css("background","transparent");
+    });
+}
+
+const $hoverPointer = () => {
+    $(".topbutton").hover(
+        function() {$(this).css('cursor','pointer');
+    }, 
+        function() {$(this).css('cursor','auto');
+    });
+    $(".push").hover(
+        function() {$(this).css('cursor','pointer');
+    }, 
+        function() {$(this).css('cursor','auto');
+    });
+}
+
+
+// FUNCTIONS CONNECTED TO THE ABOVE
     $("#home").on('click', clickHome);
     $("#aboutme").on('click', clickAboutMe);
     $("#resume").on('click', clickResume);
+    $(".topbutton").on('hover', $hoverColor())
+    $(".topbutton").on('hover', $hoverPointer())
+
     // $("#contactButton").on('click', clickContact);
 
    
@@ -23,6 +60,8 @@ $(".resume").slideDown(900);
     //     }, function(){
     //     $(this).css("background-color", "");
     //   })
+
+
     
 
     })
